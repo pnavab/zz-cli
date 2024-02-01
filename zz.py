827ad3622f1af22f22ef143b0554842a982f38e4
pnavab <114110926+pnavab@users.noreply.github.com>
2024-01-31 16:11:55 -0800

    add start .exe

--- zz.py
+++ zz.py
@@ -2,12 +2,19 @@
 import sys
 import os
 import subprocess
+import readline
 from colorama import Fore, Back, Style, init
 import zz_json as helpers
 
 init()
 def color_text(text, color):
   return f"{color}{text}{Style.RESET_ALL}"
+
+def complete_path(text, state):
+  return (path for path in os.listdir() if path.startswith(text))
+readline.set_completer_delims(' \t\n')
+readline.parse_and_bind('tab: complete')
+readline.set_completer(complete_path)
 
 def main():
   if len(sys.argv) > 1:
@@ -16,10 +23,12 @@ def main():
     elif sys.argv[1] == "open":
       if len(sys.argv) == 3:
         open(sys.argv[2])
+      elif len(sys.argv) == 2:
+        print(color_text("Missing alias in command", Fore.RED))
       elif len(sys.argv) == 4 and sys.argv[3] == "-r":
         open(sys.argv[2], reload=True)
       else:
-        print(color_text("Make sure your request is valid!", Fore.RED))
+        print(color_text("Invalid command!", Fore.RED))
         help()
     elif sys.argv[1] == "add":
       if len(sys.argv) < 4:
@@ -34,10 +43,15 @@ def main():
         delete(sys.argv[2])
       else:
         help()
+    elif sys.argv[1] == "start":
+      if len(sys.argv) < 4:
+        start(sys.argv[2])
+      else:
+        help()
     elif sys.argv[1] == "list":
       list_all()
     else:
-      print(color_text("Make sure your request is valid!", Fore.RED))
+      print(color_text("Invalid command!", Fore.RED))
       help()
   else:
     help()
@@ -45,8 +59,10 @@ def main():
 def help():
   print("="*80)
   print("Usage:")
-  print(f"   Run {color_text("'zz add <alias>'", Fore.CYAN)} to add a directory with a new alias")
-  print(f"   Run {color_text("'zz open <alias>'", Fore.CYAN)} to open a previously added directory. Append '-r' to open in the same VScode window")
+  print(f"   Run {color_text("'zz add <alias>'", Fore.CYAN)} to add a path with a new alias")
+  print(f"   Run {color_text("'zz add .'", Fore.CYAN)} to add the current path with a new alias")
+  print(f"   Run {color_text("'zz open <alias>'", Fore.CYAN)} to open a previously added directory in VScode. Append '-r' to open in the same window")
+  print(f"   Run {color_text("'zz start <alias>'", Fore.CYAN)} to run a previously added executable path")
   print("="*80)
 
 def list_all():
@@ -74,7 +90,7 @@ def add(alias):
     print(color_text(f"Alias {alias} already points to {check_dir}", Fore.RED))
     help()
     return
-  directory = input("Enter the full directory path: ")
+  directory = input("Enter the full path: ")
   if helpers.add_entry(alias, directory):
     print(color_text(f"Successfully added '{alias}' pointing to '{directory}'", Fore.GREEN))
   else:
@@ -93,6 +109,17 @@ def add_current_directory():
   else:
     print(color_text("Error adding pair", Fore.RED))
 
+def start(alias):
+  path = helpers.get_directory_from_alias(alias)
+  if path is None:
+    print(color_text(f"Alias {alias} does not point to a directory", Fore.RED))
+    return
+  if path[0] == "\"" or path[0] == "\'":
+    path = path[1:-1]
+  if path[-3:] != "exe":
+    print(color_text(f"Alias '{alias}' does not point to an executable path", Fore.RED))
+    return
+  subprocess.run([path], shell=True)
 
 def delete(alias):
   if helpers.remove_entry(alias):
