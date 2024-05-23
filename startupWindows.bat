f526800aefb42ec4bd9a0c1b0da73ab2524143b9
pnavab <114110926+pnavab@users.noreply.github.com>
2024-05-23 01:06:33 -0700

    add startup scripts

---
+++ startupWindows.bat
@@ -0,0 +1,14 @@
+@echo off
+
+REM Add the current directory to the user PATH if it is not already there
+for /f "usebackq tokens=2,*" %%A in (`reg query HKCU\Environment /v PATH`) do set my_user_path=%%B
+
+echo.%my_user_path% | findstr /C:"%cd%" 1>nul
+if errorlevel 1 (
+  setx PATH "%my_user_path%;%cd%"
+) else (
+  echo The current directory is already in the PATH.
+)
+
+echo DIRECTORY=%cd% > .env
+echo {} > pi.json
