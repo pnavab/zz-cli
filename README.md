c26ff9a131220219bd4bc45b187c1f983bc34e9b
pnavab <114110926+pnavab@users.noreply.github.com>
2024-05-23 01:05:24 -0700

    fix main py

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Run 'zz add \<alias>' to add a path with a new alias
 Run 'zz add .' to add the current path with a new alias  
 Run 'zz open \<alias>' to open a previously added directory in VScode. Append '-r' to open in the same window  
 Run 'zz start \<alias>' to run a previously added executable path  
-Run 'zz ask' to enable search mode  
+Run 'zz ask' to enable browser search mode  
 Run 'zz init' to initialize .git and commit all current content to a remote Github repository  
 Run 'zz init \<repo name>' to create a remote Github repository with the name and push all current content to it  
 Run 'zz pa' to push all code to Github using 'git add .' command"  
--- zz.py
+++ zz.py
@@ -94,10 +94,10 @@ def help():
   print(f"   Run {color_text("'zz add .'", Fore.CYAN)} to add the current path with a new alias")
   print(f"   Run {color_text("'zz open <alias>'", Fore.CYAN)} to open a previously added directory in VScode. Append '-r' to open in the same window")
   print(f"   Run {color_text("'zz start <alias>'", Fore.CYAN)} to run a previously added executable path")
-  print(f"   Run {color_text("'zz ask'", Fore.CYAN)} to enable search mode")
+  print(f"   Run {color_text("'zz ask'", Fore.CYAN)} to enable browser search mode")
   print(f"   Run {color_text("'zz init'", Fore.CYAN)} to initialize .git and commit all current content to a remote Github repository")
   print(f"   Run {color_text("'zz init <repo name>'", Fore.CYAN)} to create a remote Github repository with the name and push all current content to it")
-  print(f"   Run {color_text("'zz pa'", Fore.CYAN)} to enable pushing all code to Github with 'git add .'")
+  print(f"   Run {color_text("'zz pa'", Fore.CYAN)} to to push all code to Github using 'git add .' command")
   print(f"   Run {color_text("'zz clone <OPTIONAL: Github account>'", Fore.CYAN)} to fetch all repositories from the specified Github account and clone the selected one")
   print("="*80)
 
